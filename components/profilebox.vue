e95c82e2be622711c3ff9ace0c28be78ed2715eb
Slavo Salončuk <96195739+Slavissimo@users.noreply.github.com>
2024-02-09 15:24:53 +0100

    Profile-site (base)

---
+++ components/profilebox.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="profile">
+        <div class="profile-pic">
+            <span class="circle"></span>
+        </div>
+        <div class="profile-info">
+            <h1 class="username">Username</h1>
+            <p class="quote">"Quote"</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.profile{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 20px;
+}
+.profile-pic{
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+}
+.circle{
+    width: 250px;
+    height: 250px;
+    border-radius: 50%;
+    background-color: #2c2c2c;
+    margin-right: auto;
+    margin-left: 5%;
+}
+.username{
+    font-size: 32px;
+}
+.quote{
+    font-size: 18px;
+    font-style: italic;
+}
+.profile-info{
+    display: flex;
+    flex-direction: column;
+    margin-left: 20px;
+}
+</style>
---
+++ pages/profile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="profile-header">
+        <h1>Profile</h1>
+    </div>
+    <div>
+        <Profile />
+    </div>
+</template>
+
+<script>
+import Profile from '../components/profilebox.vue';
+export default {
+    components: {
+        Profile
+    }
+}
+</script>
+
+<style>
+.profile-header{
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    font-size: 40px;
+}
+</style>
